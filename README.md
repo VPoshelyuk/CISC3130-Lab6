5da0ee3ea66d3e82717c25eaf029015f960982ba
VPoshelyuk <aslava198@gmail.com>
2020-11-15 20:33:36 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# CISC3130-Lab6
+# CISC3130 Lab6: Hello-World
+## Create a grade calculator to calculate your grade for this class.
+
+---
+### About
+---
+This app is a very simple to use grade calculator for CISC3130 course.  
+It collects all the necessary information and provides users with some statistics regarding their performance in the course.
+
+---
+### Dependencies
+---
+- Terminal for command line access
+
+There are no packages needs to be installed to run the contents of this repo, since this is a very simple app that runs purely on HTML+CSS+JS.
+
+---
+### Setup and Usage
+---
+1. While you are in the home directory of this repo, press on the green "Code" button in the top right corner.
+2. You will be prompted to copy an SSH or HTTPS URL to clone this repo to your local machine(I personally prefer to clone over SSH, because it's more secure).
+3. After you got that URL, go to the terminal on your local machine and run the following command: 
+```zsh
+git clone *PASTE THE LINK HERE*
+```
+4. It should not tale long to copy all the files to your computer, since this is a very lightweight repo, so after it's done cd to the newly created folder by running: 
+```zsh 
+cd CISC3130-Lab6
+```  
+5. When you are inside of the CISC3130-Lab6 directory, run the following command: 
+```zsh
+open index.html
+```
+And you will be presented with the first page of the app where you will be prompted to enter your name and section
---
+++ src/index.js
@@ -0,0 +1,135 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //grab all the necessary elements by their id's
+    const personalInfoSection = document.getElementById('personal_info');
+    const septemberSection = document.getElementById('september');
+    const octoberSection = document.getElementById('october');
+    const novemberSection = document.getElementById('november');
+    const decemberSection = document.getElementById('december');
+    const finalSection = document.getElementById('final_info');
+    const next = document.getElementById('next');
+    const startOver = document.getElementById('startOver');
+
+    //array of sections for easier access
+    const allSections = [
+        personalInfoSection,
+        septemberSection,
+        octoberSection,
+        novemberSection,
+        decemberSection
+    ];
+
+    //create variables that are going to be
+    //populated with the user inputs
+    let name = '';
+    let sectionInfo = '';
+    let labs = [];
+    let practiceProblems = [];
+    let exams = [];
+
+    const handleNextClick = (e) => {
+        //iterate over sections of the form
+        allSections.forEach(section => {
+            if(section.style.display !== "none"){ //find the section that was just displayed
+                if (section.id === 'december') {
+                    //if it's the last section of the form, make all the calculations and append results to the final page
+                    const finalSec = sectionInfo !== "" ? `(Section: ${sectionInfo})` : "";//if section info was entered, use it
+                    const finalName = name !== "" ? name : "Anonymous";//check to see if user chose to be anonymous if decided to name themselves
+
+                    const labsScores = labs.length >= 5 ? //use only up to 5 first lab scores(number of labs we had so far)
+                        labs.slice(0,5).reduce((a,b) => a + b)
+                        :
+                        labs.length !== 0 ?
+                            labs.reduce((a,b) => a + b) : 0;
+                    const labsAvg = (labsScores/5).toFixed(2)//find labs average
+                    const labsMissed = labs.length < 5 ? //find number of labs missed
+                        `<p>-> You missed ${5 - labs.length} labs<p>`
+                        :
+                        "<p>-> No labs missed! Great job!<p>";
+                    const labsStats= labsAvg >= 1.4 ? 
+                        `<p>-> You are on a good track with labs!<p>`
+                        :
+                        "";
+
+                    const practiceProblemsScores = practiceProblems.length >= 6 ? //use only up to 6 first lab scores(number of practice problems we had so far)
+                        practiceProblems.slice(0,6).reduce((a,b) => a + b)
+                        :
+                        practiceProblems.length !== 0 ? 
+                            practiceProblems.reduce((a,b) => a + b) : 0;
+                    const practiceProblemsAvg = (practiceProblemsScores/6).toFixed(2);//find practice problems average
+                    const ppsStats= labsAvg >= 1.4 ? 
+                        `<p>-> You are on a good track with practice problems!<p>`
+                        :
+                        "";
+
+                    const examsScores = exams.length >= 2 ? //use only up to 2 first exams scores(number of exams we had so far)
+                    exams.slice(0,2).reduce((a,b) => a + b)
+                    :
+                    exams.length !== 0 ?
+                        exams.reduce((a,b) => a + b) : 0;
+                    const examsAvg = (examsScores/2).toFixed(2);//find exams average
+                    const examsStats= examsAvg >= 7 ? 
+                        `<p>-> You are on a good track with exams!<p>`
+                        :
+                        "";
+
+                    const scoresSum = labsScores + practiceProblemsScores + examsScores;//scores sum
+                    const maxScores = 61;
+                    const prediction = scoresSum > 55 ? 
+                        "<p>I bet you'll pass this class with a pretty good score!</p>"
+                        :
+                        scoresSum < 26 ? 
+                            "<p>Looks like there is no way for you to pass this class. Sorry!</p>"
+                            :
+                            `<p>You just need ${65 - scoresSum} to pass this class!</p>`
+
+                    finalSection.innerHTML = //append everything to the last page
+                    "<p>Final Results</p></br>" +
+                    `<p>Hi, ${finalName + finalSec}</p>` +
+                    "<h1>Labs Stats:<h1>" + 
+                    `<p>-> There's been only 5 labs so far and your average score is:` + 
+                    `${labsAvg} (Maximum possible is: 1.80 w/o EC)</p>` +
+                    labsMissed + labsStats +
+                    "<h1>Practice Problems Stats:<h1>" +
+                    `<p>-> 6 practice problems sets were assigned so far and your average` +
+                    `score is: ${practiceProblemsAvg} (Maximum possible is: 5.33 w/o EC)</p>` +
+                    ppsStats +
+                    "<h1>Exams Stats:<h1>" + 
+                    `<p>-> Your midterms average is: ${examsAvg} (Maximum possible is: 10 w/o EC)</p>` +
+                    examsStats +
+                    "<h1>Notes:<h1>" +
+                    `<p>-> Your current score for the class is ${scoresSum} out of ${maxScores}</p>` +
+                    prediction;
+                } else {
+                    section.childNodes.forEach(node => {//go through all the nodes of the section
+                        if(node.nodeName === 'INPUT'){//choose only input fields
+                            if (node.id.includes('lab') && node.value) {//if it's a score for lab, append to lab array
+                                labs.push(parseFloat(node.value));
+                            } else if (node.id.includes('pp') && node.value) {//if it's a score for practice problems, append to practice problems array
+                                practiceProblems.push(parseFloat(node.value));
+                            } else if (node.id.includes('exam') && node.value) {//if it's a score for exam, append to exam array
+                                exams.push(parseFloat(node.value))
+                            } else if (node.id.includes('name')) {//if it's a name, save it
+                                name += name === '' ? node.value : ` ${node.value}`
+                            } else if (node.id === 'section') {//save section input
+                                sectionInfo = node.value;
+                            }
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    //clear everything on "Start Over" button click
+    const handleStartOverClick = (e) => {
+        labs = [];
+        practiceProblems = [];
+        exams = [];
+        name = '';
+        sectionInfo = '';
+    }
+
+    //add listeners to the only 2 buttons of this app
+    next.addEventListener("click", handleNextClick);
+    startOver.addEventListener("click", handleStartOverClick);
+})
